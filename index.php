e4972713b6586549ce6b6358602d33911a33adbb
diegoa <diegoamarilla3@gmail.com>
2021-08-05 16:29:11 -0400

    inicio_proyecto

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+		<meta charset="iso-8559-1">
+		<meta http-equiv="Expires" content="0">
+  		<meta http-equiv="Last-Modified" content="0">
+  		<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  		<meta http-equiv="Pragma" content="no-cache">
+  		<base href="http://<?php echo $_SERVER['SERVER_NAME'].str_replace("index","",substr($_SERVER['SCRIPT_NAME'],0,-4));?>" />
+  		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+  		<meta name="author" content="">
+
+		<title></title>
+	</head>
+	<body>
+		<h1>Control de cuotas</h1>
+		<table>
+			<tr>
+				<td>Mes</td>
+				<td>Cuota nro</td>
+				<td>Monto pagado</td>
+				<td>Falta</td>
+				<td>Descuento</td>
+				<td>Monto con desc</td>
+
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+
+
+			</tr>
+		</table>
+	</body>
+</html>
